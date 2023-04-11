fac70547f6d072e36e9ca3a0b8e7e1ac0a788d58
Jashandeep Kaur <jashanaulakh175@gmail.com>
2023-04-10 20:32:25 -0400

    initial commit

---
+++ js/data.js
@@ -0,0 +1,28 @@
+window.reviewData = [
+
+    {
+        date: "2023/01/07",
+        review: "Badminton is really a nice game. I love playing this game along with my sister.",
+        rate: "3/5"
+    },
+    {
+        date: "2023/01/15",
+        review: "This website is amazing and provides the information in a very good manner",
+        rate: "4/5"
+    },
+    {
+        date: "2023/02/02",
+        review: "Love this game",
+        rate: "5/5"
+    },
+    {
+        date: "2023/02/03",
+        review: "I have a lot of my childhood memories connected with this game",
+        rate: "5/5"
+    },
+    {
+        date: "2023/02/25",
+        review: "Peace of Mind",
+        rate: "5/5"
+    }
+];
